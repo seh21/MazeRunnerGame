1440c81aa25a511c0dd40cec6aebc0c2e23b0022
seh21 <seh21@vt.edu>
2015-12-06 16:42:48 -0500

    Update game.py

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ minotaur_y = 1
 thes_x = 1
 thes_y = 7
 
-state = -1 # states of state machine
+state = 0 # states of state machine
 endGame = 0 # boolean for end of game 
 key = -1 # user input key selection
 
@@ -22,16 +22,15 @@ while endGame == 0:
 		print("Use the keys A, S, W, and D to navigate\nthrough the maze. You must\n")
 		print("move Theseus (~) to the exit '>'\nwithout getting eaten by the Minotaur (<).\n")
 		print("To begin, press G. To exit, press E.")
-	key = getch.getch() # gets user input 
-	if key == 'G':
-		# clear the screen 
-		state = 1
-	elif key = 'E':
-		endGame = 1
-		break
-	else;
-		state = 0 
-
+		key = getch.getch() # gets user input 
+		if key == 'G':
+			# clear the screen 
+			state = 1
+		elif key = 'E':
+			endGame = 1
+			break
+		else;
+			state = 0 
 	if state == 1:
 		# initialize and generate map 
 		state = 2
@@ -42,11 +41,16 @@ while endGame == 0:
 		dir = getch.getch()
 		
 		if dir == 'd'
+			thes_x = thes_x + 1 
 		elif dir == 's'
+			thes_y = thes_y - 1 
 		elif dir == 'a'
+			thes_x = thes_x - 1
 		elif dir == 'w'
+			thes_y = thes_y + 1
 		elif dir == 'E'
 			# exit game 
 			break 
+		
 		
 		
