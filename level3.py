eb53436055928dd037c580456a073e3ea26ef374
thesmith4734 <sgreg@vt.edu>
2015-12-08 19:15:19 -0500

    level 3
    the rest is commin'

---
+++ level3.py
@@ -0,0 +1,45 @@
+def makeLevel3():
+	maze = [[ " " for j in range(9)] for i in range(20)]
+	
+	for j in range(0, 19):
+		maze[j][8] = "#"
+	for j in range(0, 19):
+		maze[j][0] = "#"
+	for j in range(0, 9):
+		maze[0][j] = "#"
+	for j in range(0, 9):
+		maze[19][j] = "#"
+	for j in range(1, 3):
+		maze[j][2] = "#"
+	for j in range(1, 7):
+		maze[4][j] = "#"
+	for j in range(2, 4):
+		maze[j][6] = "#"
+	for j in range(2, 8):
+		maze[9][j] = "#"
+	maze[5][5] = "#"
+	maze[8][5] = "#"
+	for j in range(2, 7):
+		maze[15][j] = "#"
+	for j in range(10, 14):
+		maze[j][4] = "#"
+	maze[10][5] = "#"
+	
+	for j in range(11, 15):
+		maze[j][6] = "_"
+	for j in range(10, 14):
+		maze[j][2] = "_"
+	for j in range(2, 7):
+		maze[16][j] = "_"
+	maze[17][6] = "_"
+	maze[6][3] = "_"
+	maze[7][2] = "_"
+	maze[8][2] = "_"
+	maze[6][5] = "_"
+	maze[6][7] = "_"
+	maze[2][4] = "_"
+	maze[10][8] = " "
+	maze[1][1] = "0"
+		
+	return maze
+ 
