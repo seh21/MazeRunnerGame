d96489640f7bdec0989ccb7989babcd6e11a04d4
thesmith4734 <sgreg@vt.edu>
2015-12-08 22:14:23 -0500

    Added levers and traps
    still need levers for lvl 5

--- movement.py
+++ movement.py
@@ -25,4 +25,37 @@ def updateMap(x_val, y_val, x_val_last, y_val_last, currentMap):
 		for val in row:
 			print '{:4}'.format(val),
 		print
-	return currentMap	
+	return currentMap
+
+def trapCheck(x_val, y_val, currentMap):
+	if currentMap[y_val][x_val] == "_":
+		currentMap[y_val][x_val] = "^"
+		value = 0
+	else:
+		value = 1
+	return value
+
+def leverCheck(x_val, y_val, currentMap, level, stage):
+	if currentMap[y_val][x_val] == "L":
+		if level == "4":
+			if stage == 1:
+				currentMap[2][4] = " "
+			elif stage == 2:
+				currentMap[5][2] = " "
+			elif stage == 3:
+				currentMap[10][2] = " "
+			elif stage == 4:
+				currentMap[15][2] = " "
+			elif stage == 5:
+				currentMap[17][4] = " "
+			elif stage == 6:
+				currentMap[5][6] = " "
+			elif stage == 7:
+				currentMap[15][6] = " "
+			elif stage == 8:
+				currentMap[10][7] = " "
+				currentMap[11][7] = "#"
+			stage += 1
+
+	return currentMap, stage
+		
