87664af23bd93ea075096c70c000ac355c1f4595
Zach Taylor <zeekster@vt.edu>
2015-12-09 16:07:39 -0500

    Function for Level #4

--- level4.py
+++ level4.py
@@ -1,6 +1,9 @@
+# Level 4 for Maze Runner Game
 def makeLevel4():
+	# Definition of Range
 	maze = [[ " " for j in range(9)] for i in range(20)]
 
+	# Use of '#' to show boundary
 	for j in range(0, 19):
 		maze[j][8] = "#"
 	for j in range(0, 19):
@@ -18,6 +21,7 @@ def makeLevel4():
 	for j in range(1, 19):
 		maze[j][4] = "#"
 	
+	# Use of 'L' to show Trap
 	maze[3][2] = "L"
 	maze[3][6] = "L"
 	maze[8][2] = "L"
@@ -27,8 +31,10 @@ def makeLevel4():
 	maze[17][2] = "L"
 	maze[17][6] = "L"
 	
+	# Use of ' ' to show exit
 	maze[10][8] = " "
+	
+	# Use of '0' to show player
 	maze[1][1] = "0"
 
 	return maze
- 
